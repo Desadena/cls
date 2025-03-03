3e4c323b425b7257f483b6589150751fbc6d3a16
Desadena <164672768+Desadena@users.noreply.github.com>
2025-03-03 19:26:58 +0100

    Error debugged
    "os.system("cls")"
    prints "0"
    With this change it won´t

--- packages_cls/src/cls/__init__.py
+++ packages_cls/src/cls/__init__.py
@@ -3,6 +3,7 @@ import sys as system
 
 def clear():
     if system.platform.startswith(('win32')):
-        os.system('cls')
+        cls_v = os.system('cls')
+        del cls_v
     elif system.platform.startswith(('linux', 'cygwin', 'darwin', 'freebsd')):
         os.system('clear')
